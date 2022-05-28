27b23efa9dbaf6e28aa8e8e31af0c81a25af74cb
Kelvin <kelvs.r@outlook.com>
2022-05-28 15:43:09 +0800

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Kelvin Ricafort on 5/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
